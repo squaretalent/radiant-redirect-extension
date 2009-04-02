cd9204406b9a2a5b3b8fc580a68d19a721cd157c
Petrik <petrik@deheus.net>
2009-04-02 16:00:14 +0200

    Added code

---
+++ app/models/redirect_page.rb
@@ -0,0 +1,6 @@
+class RedirectPage < Page
+
+  description %{
+    A redirect page will redirect to the url defined in the body part
+  }
+end
---
+++ lib/page_redirect/controller_extensions.rb
@@ -0,0 +1,24 @@
+module PageRedirect::ControllerExtensions
+  def self.included(base)
+    base.class_eval { before_filter :apply_page_redirects }
+  end
+  
+  def apply_page_redirects
+    url = params[:url]
+    if Array === url
+      url = url.join('/')
+    else
+      url = url.to_s
+    end
+    a_match = Page.find_by_url(url, live?)    
+    if a_match && a_match.is_a?(RedirectPage) 
+      body = a_match.parts.find_by_name("body")
+      if body
+        redirect_url = body.content
+        location = redirect_url.match('http://') ? redirect_url : url_for(:controller => 'site', :action => 'show_page', :url => redirect_url)
+        redirect_to CGI.unescape(location), :status => '307 Temporarily Moved' and return
+      end
+    end
+  end
+
+end
