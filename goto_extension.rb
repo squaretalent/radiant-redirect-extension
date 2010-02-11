f574bb017308ee8f89d5eaee8f702d1463c6f7e4
dirkkelly <dirk.kelly@squaretalent.com>
2010-02-11 15:33:07 +0800

    renamed to goto, tested in 0.9.0 (working)

--- goto_extension.rb
+++ goto_extension.rb
@@ -1,7 +1,7 @@
 # Uncomment this if you reference any of your controllers in activate
 # require_dependency 'application'
 
-class RedirectPageExtension < Radiant::Extension
+class GotoExtension < Radiant::Extension
   version "0.1"
   description "A redirect page will redirect to the url defined in the body part"
   url "http://github.com/p8/radiant-redirect-page-extension"
