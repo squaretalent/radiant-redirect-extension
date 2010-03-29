9ea39d4f748b1dcd348b23677500ed7e1a2763ae
dirkkelly <dirk.kelly@squaretalent.com>
2010-03-29 12:15:33 +0800

    renaming to redirect, so it is slightly obvious that this is a branch extension

--- redirect_extension.rb
+++ redirect_extension.rb
@@ -1,19 +1,16 @@
 # Uncomment this if you reference any of your controllers in activate
 # require_dependency 'application'
 
-class GotoExtension < Radiant::Extension
+class RedirectExtension < Radiant::Extension
   version "0.1"
   description "A redirect page will redirect to the url defined in the body part"
-  url "http://github.com/p8/radiant-redirect-page-extension"
+  url "http://github.com/squaretalent/radiant-redirect-extension"
   
   def activate
     SiteController.send :include, PageRedirect::ControllerExtensions
     Page.class_eval do
       include RedirectPageTags
     end
-  end
-  
-  def deactivate
   end
   
 end
